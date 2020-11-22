5db14840f96a560ba00f68a8a022c567b1c6b62f
Neeraj Bhatia <nick@Neerajs-MBP.neo.rr.com>
2020-11-21 20:37:40 -0500

    Add some more tests

--- auth_controller.js
+++ auth_controller.js
@@ -1,7 +1,10 @@
 `use strict`
 
 module.exports = {
-    login:(us, pw) => {
-        return `true`;
+    login:(un, pw) => {
+        if (un && pw && un === `username` && pw === `1234`){
+            return true;
+        }
+        return false; //return a 401
     }
 };
--- tests/auth_controller.test.js
+++ tests/auth_controller.test.js
@@ -3,5 +3,13 @@
 const auth_controller = require(`../auth_controller`);
 
 test (`returns false for invalid user credentials`, () => {
-    expect(auth_controller.login(`pat`, `1235`)).toBe(false);
+    expect(auth_controller.login(`username`, `1235`)).toBe(false);
+});
+
+test (`returns false for missing user credentials`, () => {
+    expect(auth_controller.login()).toBe(false);
+});
+
+test (`returns false for invalid user credentials`, () => {
+    expect(auth_controller.login(`username`, `1234`)).toBe(true);
 });
